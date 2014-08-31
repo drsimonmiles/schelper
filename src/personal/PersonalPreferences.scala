a9dc6458edd29ed29ad788fa4f577b9a79ed5559
Simon Miles <simon.miles@kcl.ac.uk>
2014-08-31 20:53:48 +0100

    Personal preferences list

---
+++ src/personal/PersonalPreferences.scala
@@ -0,0 +1,11 @@
+import schelper.core.SchelperConstants.ActionColour
+import schelper.core.PreferencesList
+import schelper.navigate.Navigable
+
+object PersonalPreferences extends PreferencesList (Map (
+  "Citing papers file" -> "ListPath",
+  "Paper signatures file" -> "PaperSignatures"  // ADD ITEMS HERE
+)) with Navigable {
+  val linkName = "Set preferences"
+  val linkColour = ActionColour
+}
