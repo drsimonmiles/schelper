9bd51dad8b0090dd3c8727abd9f0a893dfe240e8
Simon Miles <simon.miles@kcl.ac.uk>
2015-06-25 07:56:40 +0100

    Added function to save budget spreadsheets

---
+++ src/personal/BudgetDirectories.scala
@@ -0,0 +1,17 @@
+package personal
+
+import schelper.core.PreferencesList
+import schelper.core.SchelperConstants._
+import schelper.navigate.Navigable
+
+object BudgetDirectories extends PreferencesList (Map (
+  "CCSAABR" -> "CCSAABRDirectory",
+  "CCSLAHR" -> "CCSLAHRDirectory",
+  "CCSYACR" -> "CCSYACRDirectory",
+  "MHFYAKR" -> "MHFYAKRDirectory",
+  "MHFLBAR" -> "MHFLBARDirectory",
+  "MHFRBGR" -> "MHFRBGRDirectory"
+)) with Navigable {
+  val linkName = "Set budget directories"
+  val linkColour = ActionColour
+}
--- src/schelper/core/PreferencesList.scala
+++ src/schelper/core/PreferencesList.scala
@@ -7,17 +7,13 @@ import schelper.navigate.BackButton
 
 class PreferencesList (labelToPreference: Map[String, String]) extends BasicForm {
   val fields: Map[String, TextField] =
-    (for ((label, preference) <- labelToPreference) yield
+    for ((label, preference) <- labelToPreference) yield
       (preference, field (label, getString (preference).getOrElse ("")))
-    ).toMap
   val saveButton = button (new BackButton ("Save", () => save (), ActionColour))
   val cancelButton = button (new BackButton ("Cancel", NavigateColour))
   val componentPublishers = saveButton :: cancelButton :: fields.values.toList
 
-  def save () {
-    for (preference <- labelToPreference.values) {
-      println ("Saving " + preference + " to be value " + fields (preference).text)
+  def save (): Unit =
+    for (preference <- labelToPreference.values)
       putString (preference, fields (preference).text)
-    }
-  }
 }
